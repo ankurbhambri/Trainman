66eed1bcfcb2ea8a445ecf74bf1d4355c8311fd9
Ankur Bhambri <ankur.bhambri@renewbuy.com>
2020-09-01 23:25:49 +0530

    allowed hosts

--- trainman/settings.py
+++ trainman/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g7h(g04ki1x*sowze4p*vr@uob%g@b52lyp-k-7n6u2)&3qs5o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['fast-caverns-34044.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
